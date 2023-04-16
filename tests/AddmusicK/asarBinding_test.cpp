b51c1f9d97cbe1565abdfa3d49e15892d4581aa0
VirusRushTheater <virusrushtheater@gmail.com>
2023-04-15 21:05:33 -0400

    Tidying the build environment

--- tests/AddmusicK/asarBinding_test.cpp
+++ tests/AddmusicK/asarBinding_test.cpp
@@ -2,7 +2,9 @@
 #include <catch2/catch.hpp>
 
 #include "asarBinding.h"
+#include "Utility.h"
 
-TEST_CASE("Test add_music function", "[add_music][core]") {
+TEST_CASE("asarBinding stdout/stderr test", "[addmusick][asarbinding][compilation]")
+{
     REQUIRE(true);
 }
