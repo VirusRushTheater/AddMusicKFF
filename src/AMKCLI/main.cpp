61ade2d00b6899add9418b0ddd9355ae38063987
VirusRushTheater <virusrushtheater@gmail.com>
2023-05-06 22:38:57 -0400

    Starting work on CLI

---
+++ src/AMKCLI/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "AddmusicK.h"
+
+int main (int argc, char** argv)
+{
+	
+	return 0;
+}
---
+++ src/AddmusicK/AddmusicK.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "defines.h"
+#include "SPCEnvironment.h"
+#include "ROMEnvironment.h"
